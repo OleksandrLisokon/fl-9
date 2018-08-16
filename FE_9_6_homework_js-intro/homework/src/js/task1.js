8a878d5a73098a33f12c5a35fadc8864012a9bb3
OleksandrLisokon <lisokon85@gmail.com>
2018-08-16 20:02:11 +0300

    Homework JS Intro

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,5 +1,12 @@
 let price = parseFloat(prompt('Please enter the price: '));
 let discount = parseFloat(prompt('Please enter the discount: '));
-isNaN(price) || isNaN(discount) || price <= 0 || discount <0 || discount >100 ? console.log('Invalid data') : 
-console.log('Price without discount: ' + price + '\nDiscount: ' + discount + '%\nPrice with discount: ' + 
-+(price - price * (discount/100)).toFixed(2) + '\nSaved: ' + +(price * (discount/100)).toFixed(2));
+let priceWithDiscount = +(price - price * (discount/100)).toFixed(2);
+
+if (isNaN(price) || isNaN(discount) || price <= 0 || discount < 0 || discount > 100) {
+    console.log('Invalid data');
+} else {
+    console.log('Price without discount: ' + price + 
+                '\nDiscount: ' + discount + 
+                '%\nPrice with discount: ' + priceWithDiscount +
+                '\nSaved: ' + +(price - priceWithDiscount).toFixed(2));
+        } 
--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -3,8 +3,13 @@ let a = parseFloat(prompt('Please enter a length: '));
 let b = parseFloat(prompt('Please enter b length: '));
 let angle = parseFloat(prompt('Please enter α- angle: ')) / maxAngle * Math.PI;
 let c = Math.sqrt(a*a + b*b - 2*a*b*Math.cos(angle));
+let square = a * b * Math.sin(angle) / 2;
 
-isNaN(a) || isNaN(b) || isNaN(angle) || a.toFixed(2) <= 0 || b.toFixed(2) <=0 || angle.toFixed(2) <=0 
-|| angle.toFixed(2) >= maxAngle ? console.log('Invalid data') : 
-console.log('c length: ' + +c.toFixed(2) + '\nTriangle square: ' + +(a * b * Math.sin(angle) / 2).toFixed(2)
- + '\nTriangle perimeter: ' + +(a + b + c).toFixed(2));
+if (isNaN(a) || isNaN(b) || isNaN(angle) || a.toFixed(2) <= 0) {
+    console.log('Invalid data');
+} else if (b.toFixed(2) <=0 || angle.toFixed(2) <=0 || angle.toFixed(2) >= maxAngle) {
+    console.log('Invalid data');
+} else {
+    console.log('c length: ' + +c.toFixed(2) + '\nTriangle square: ' + +square.toFixed(2)
+    + '\nTriangle perimeter: ' + +(a + b + c).toFixed(2));
+}
