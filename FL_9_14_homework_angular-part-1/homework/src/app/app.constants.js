f9048f77113b8f4157ceac63ec07eb3114d25f5d
OleksandrLisokon <lisokon85@gmail.com>
2018-11-11 15:25:58 +0200

    FL9. Offline. Lesson 14. Angular Part 1

---
+++ FL_9_14_homework_angular-part-1/homework/src/app/app.constants.js
@@ -0,0 +1,58 @@
+const wallPosts = [
+    {
+        "title":"First Contact",
+        "message":"Testing, testing, 1-2-3... wait, that doesn't actually work here. First of all, I have to "+
+        "introduce myself. I am someone, yet I am no one. No, I won’t disclose my real life, I just want you "+
+        "to read what I write, enjoy what I create and (most importantly!) leave comments when I am making "+
+        "mistakes.",
+        "author_name":"Valdemar Kristensen",
+        "author_photo":"assets/img/man1.jpg",
+        "likes":3,
+        "liked":true,
+        "id":0,
+    },
+    {
+        "title":"How not to write a blog post",
+        "message":"Didn't I say a post will come soon? Well, here I am again. Disappointingly, this won’t be "+
+        "a very long post, as I didn’t really have much time to think about what I will write. As I am writing"+
+        " this, the title field is still empty, so this might be a clue to how clueless I am.",
+        "author_name":"Morris Fields",
+        "author_photo":"assets/img/man2.jpg",
+        "likes":2,
+        "liked":false,
+        "id":1,
+    },
+    {
+        "title":"Take a break",
+        "message":"I think I owe you a proper post, don't I? Well, I think the topic will be of interest to a "+
+        "decent portion of the online community. Before I start defending video games, I must say that I do "+
+        "enjoy playing them. I don’t overdo it, but I do play a couple of hours during the evening. Also, I "+
+        "don’t scrap plans just to have my daily dose of games, there are a lot of other things more important "+
+        "than them.",
+        "author_name":"Priscilla Wilson",
+        "author_photo":"assets/img/woman1.jpg",
+        "likes":10,
+        "liked":false,
+        "id":2,
+    },
+    {
+        "title":"A trip to Wonderland",
+        "message":"Anyone who physically went to a magical place please raise your hand! Anyone? Now, who "+
+        "mentally went to a magical place? Now that’s more like it! For those of you who didn’t catch this "+
+        "train of thought, I’m talking about books. No, not those ones that you were forced (or still are "+
+        "forced, I wouldn’t want to be in your place) to read in school, those are generally boring. Well, "+
+        "maybe they might be found by people who actually enjoy them, but I think less than 5% of the people "+
+        "I know that have read those books actually liked them, so… the argument still stands.",
+        "author_name":"Suzy Simmmons",
+        "author_photo":"assets/img/woman2.jpg",
+        "likes":7,
+        "liked":false,
+        "id":3,
+    }
+];
+
+const user = {
+    "name":"Oleksandr Lisokon",
+    "photo":"assets/img/user.jpg",
+};
+
